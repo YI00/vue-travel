cf14da530e7dccb06ff82e6233d467349821056d
YI <1208655946@qq.com>
2019-10-05 09:26:13 +0800

    ajax

--- vue-travel/src/pages/home/components/Recommend.vue
+++ vue-travel/src/pages/home/components/Recommend.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="title">热销推荐</div>
     <ul>
-      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+      <li class="item border-bottom" v-for="item of list" :key="item.id">
         <img class="item-img" :src="item.imgUrl" alt="">
         <div class="item-info">
           <p class="item-title">{{item.title}}</p>
@@ -16,47 +16,8 @@
 <script>
 export default{
   name:'HomeRecommend',
-  data(){
-    return {
-      recommendList:[
-        {
-          id:'0001',
-          imgUrl:'https://imgs.qunarzz.com/sight/p0/1804/1a/1a48cc20b0d6ed44a3.img.jpg_200x200_32e22bed.jpg',
-          desc:'浪漫大连首站，浪漫的海洋主题乐园',
-          title:"大连圣亚海洋世界"
-        },
-        {
-          id:'0002',
-          imgUrl:'https://imgs.qunarzz.com/sight/p0/1804/1a/1a48cc20b0d6ed44a3.img.jpg_200x200_32e22bed.jpg',
-          desc:'浪漫大连首站，浪漫的海洋主题乐园',
-          title:"大连圣亚海洋世界"
-        },
-        {
-          id:'0003',
-          imgUrl:'https://imgs.qunarzz.com/sight/p0/1804/1a/1a48cc20b0d6ed44a3.img.jpg_200x200_32e22bed.jpg',
-          desc:'浪漫大连首站，浪漫的海洋主题乐园',
-          title:"大连圣亚海洋世界"
-        },
-        {
-          id:'0004',
-          imgUrl:'https://imgs.qunarzz.com/sight/p0/1804/1a/1a48cc20b0d6ed44a3.img.jpg_200x200_32e22bed.jpg',
-          desc:'浪漫大连首站，浪漫的海洋主题乐园',
-          title:"大连圣亚海洋世界"
-        },
-        {
-          id:'0005',
-          imgUrl:'https://imgs.qunarzz.com/sight/p0/1804/1a/1a48cc20b0d6ed44a3.img.jpg_200x200_32e22bed.jpg',
-          desc:'浪漫大连首站，浪漫的海洋主题乐园',
-          title:"大连圣亚海洋世界"
-        },
-        {
-          id:'0006',
-          imgUrl:'https://imgs.qunarzz.com/sight/p0/1804/1a/1a48cc20b0d6ed44a3.img.jpg_200x200_32e22bed.jpg',
-          desc:'浪漫大连首站，浪漫的海洋主题乐园',
-          title:"大连圣亚海洋世界"
-        }
-      ]
-    }
+  props:{
+    list:Array
   }
 }
 </script>
--- vue-travel/src/pages/home/components/Swiper.vue
+++ vue-travel/src/pages/home/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="wrapper">
-      <swiper :options="swiperOption">
-        <swiper-slide v-for="item of swiperList" :key="item.id">
+      <swiper :options="swiperOption" v-if="showSwiper">
+        <swiper-slide v-for="item of list" :key="item.id">
           <img class="swiper-img" :src="item.imgUrl" alt="">
         </swiper-slide>
         <div class="swiper-pagination"  slot="pagination"></div>
@@ -11,22 +11,20 @@
 <script>
 export default {
   name:'HomeSwiper',
+  props:{
+    list:Array
+  },
   data(){
     return {
       swiperOption:{
         pagination:'.swiper-pagination',
         loop:true
-      },
-      swiperList:[
-        {
-          id:'0001',
-          imgUrl:"https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20199/839ae28f89f78168f17aaa4f137c7d42.jpg_750x200_598f53cb.jpg"
-        },
-        {
-          id:'0002',
-          imgUrl:'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20199/47f7d9e9fb7c5cb88a1245180c9ca5b4.jpg_750x200_de4858e1.jpg'
-        }
-      ]
+      }
+    }
+  },
+  computed:{
+    showSwiper(){
+      return this.list.length
     }
   }
 }
@@ -47,7 +45,7 @@ export default {
     width:100%
     height:0
     overflow:hidden
-    padding-bottom:26.7%
+    padding-bottom:31.25%
     background-color #eee
     .swiper-img
       width:100%
