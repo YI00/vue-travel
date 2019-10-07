d2d8c6aee2e8ec7baedcd5cbbb4a39ceafd6992a
YI <1208655946@qq.com>
2019-10-07 09:23:01 +0800

    components

--- vue-travel/src/pages/city/components/List.vue
+++ vue-travel/src/pages/city/components/List.vue
@@ -17,7 +17,11 @@
         </div>
       </div>
     </div>
-    <div class="area" v-for="(items,key) of cities" :key="key">
+    <div class="area" 
+         v-for="(items,key) of cities" 
+         :key="key"
+         :ref="key"
+    >
       <div class="title border-topbottom">
         {{key}}
       </div>
@@ -38,10 +42,20 @@ export default {
   name:'CityList',
   props:{
     cities:Object,
-    hot:Array
+    hot:Array,
+    letter:String
   },
   mounted(){
     this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch:{
+    letter(){
+      if(this.letter){
+        const element = this.$refs[this.letter][0]
+        // console.log(element)
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
