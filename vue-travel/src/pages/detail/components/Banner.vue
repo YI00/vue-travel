c2873703c1add8029856eea2e9fde3cd1049b27a
YI <1208655946@qq.com>
2019-10-08 10:13:28 +0800

    banner finsh

---
+++ vue-travel/src/pages/detail/components/Banner.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img"
+        src="//img1.qunarzz.com/sight/p0/1804/1a/1a48cc20b0d6ed44a3.img.jpg_600x330_3f524da7.jpg">
+      <div class="banner-info">
+          <div class="banner-title">大连圣亚海洋世界(AAAA景区)</div>
+          <div class="banner-number">
+            <span class="iconfont banner-icon">&#xe632;</span>
+            39
+          </div>
+      </div>
+    </div>
+    <common-gallary 
+        :imgs="imgs" 
+        v-show="showGallary"
+        @close="handleGallaryClose"
+    >
+    </common-gallary>
+  </div>
+</template>
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name:'DetailBanner',
+  data(){
+    return{
+      showGallary:false,
+      imgs:[
+        'http://img1.qunarzz.com/sight/p0/1804/1a/1a48cc20b0d6ed44a3.img.jpg_r_800x800_9b21ceda.jpg',
+        'http://img1.qunarzz.com/sight/p0/1804/34/34787df3fb2592e8a3.img.jpg_r_800x800_65b51a1f.jpg'
+      ]
+    }
+  },
+  components:{
+    CommonGallary
+  },
+  methods:{
+    handleBannerClick(){
+      this.showGallary = true
+    },
+    handleGallaryClose(){
+      this.showGallary = false
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow:hidden
+    height:0
+    padding-bottom:55%
+    .banner-img
+      width:100%
+    .banner-info
+      display:flex
+      position:absolute
+      left:0
+      right:0
+      bottom:0
+      line-height:.6rem
+      color:#fff
+      background-image:linear-gradient(top,rgba(0,0,0,0),rgba(0,0,0,0.8))
+      .banner-title
+        flex:1
+        font-size:.32rem
+        padding:0 .2rem
+      .banner-number
+        padding:0 .4rem
+        margin-top:.14rem
+        height:.32rem
+        line-height:.4rem
+        border-radius:.2rem
+        background:rgba(0,0,0,.8)
+        font-size:.24rem
+        .banner-icon
+          font-size 0.24rem
+</style>
